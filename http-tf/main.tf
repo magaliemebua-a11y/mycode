aebc35dd924f9eddf5e08c08443471e6fb567be1
magaliemebua-a11y <magalie.mebua@gmail.com>
2026-08-13 20:45:08 +0000

    terraform null resource loops

--- http-tf/main.tf
+++ http-tf/main.tf
@@ -1,7 +1,7 @@
 # The following example shows how to issue an HTTP GET request supplying
 # an optional request header.
 data "http" "iss" {
-  url = "http://api.open-notify.org/astros.json"      // API to send HTTP GET to
+  url ="https://pokeapi.co/api/v2/pokemon/pikachu/" // "http://api.open-notify.org/astros.json"      // API to send HTTP GET to
 
   # Optional request headers
   request_headers = {
